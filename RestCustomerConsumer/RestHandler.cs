da6b3ed7eb8897addbfda73e54e1b215824918e7
KasperHoulind <32668404+KasperHoulind@users.noreply.github.com>
2018-10-29 16:56:12 +0100

    Initial commit

---
+++ RestCustomerConsumer/RestHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using ModelClassCustomer.model;
+using Newtonsoft.Json;
+
+namespace RestCustomerConsumer
+{
+    class RestHandler
+    {
+        public IList<Customer> GetCustomers()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = client.GetStringAsync("http://localhost:61404/api/Customers").Result;
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
+    }
+}
+
+
---
+++ RestCustomerService/Controllers/CustomersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelClassCustomer.model;
+
+namespace RestCustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private static List<Customer> cList = new List<Customer>()
+        {
+            new Customer(){Id = 1234, FirstName = "Jens", LastName = "Jensen", YearOfreg = 1988},
+            new Customer(){Id = 7895, FirstName = "Tyga", LastName = "Tasten", YearOfreg = 1999},
+            new Customer(){Id = 5555, FirstName = "Stuart",LastName = "Stardust", YearOfreg = 2000}
+        };
+
+        // GET: api/Customers
+        [HttpGet]
+        //[Route("api/Customers")]
+        public IEnumerable<Customer> GetAll()
+        {
+            return cList;
+        }
+
+        //GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public void Start()
+        {
+            
+        }
+    }
+}
